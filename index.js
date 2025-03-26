c5b6216e5c927932f9f9513fc1f58fc44bb149e3
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-26 18:35:58 +0100

    fix: console.log

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const argv = process.argv[2];
-(argv === undefined) ? console.log('Hello World!') : console.log(process.argv[2]);
+// (argv === undefined) ? console.log('Hello World!') : console.log(process.argv[2]);
+console.log( (argv === undefined) ? 'Hello World!' : process.argv[2] );
